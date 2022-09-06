6e72ba105b984636126d890aa2a05d9d8f48ef1e
h2lo <olusha.98@mail.ru>
2022-09-06 22:43:05 +0300

    feat: add 18 task

---
+++ 18.js
@@ -0,0 +1,9 @@
+// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+// Change code below this line
+
+const someAdjective = "interesting, exciting, cool!";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective
+
+console.log(myStr);
