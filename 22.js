f8f49b124f20c812af8cba182c0cf4d3370cc820
h2lo <olusha.98@mail.ru>
2022-09-06 22:56:41 +0300

    feat: add 22 task

---
+++ 22.js
@@ -0,0 +1,9 @@
+//The thirdLetterOfLastName variable should have the value of v. You should use bracket notation.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+console.log(thirdLetterOfLastName);
