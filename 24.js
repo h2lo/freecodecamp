17c9373efd163a914f1522fd2b537cae86b96255
h2lo <olusha.98@mail.ru>
2022-09-06 23:06:17 +0300

    feat: add 24 task

---
+++ 24.js
@@ -0,0 +1,10 @@
+// secondToLastLetterOfLastName should be the letter c.
+// Passed:You should use .length to get the second last letter.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+console.log(secondToLastLetterOfLastName);
