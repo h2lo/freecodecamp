9fa759e69ba705258757b0cf985fcf5b21f513be
h2lo <olusha.98@mail.ru>
2022-09-06 23:00:31 +0300

    feat: add 23 task

---
+++ 23.js
@@ -0,0 +1,8 @@
+// lastLetterOfLastName should be the letter e. You should use .length to get the last letter.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+console.log(lastLetterOfLastName);
