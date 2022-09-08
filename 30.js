5072a6f66e20a882fb5c152e8c2a6bc0da8fc811
h2lo <olusha.98@mail.ru>
2022-09-08 20:14:16 +0300

    feat: add 30 task

---
+++ 30.js
@@ -0,0 +1,15 @@
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    return result = "Bigger than 5";
+  } else {
+    return result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;;
+}
+
+testElse(4);
