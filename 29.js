b5f7373880771e917e016e434e6b569e4486c2a0
h2lo <olusha.98@mail.ru>
2022-09-08 20:05:53 +0300

    feat: add 29 task

---
+++ 29.js
@@ -0,0 +1,11 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+  if (val > 11 && val < 20) {
+    return "Inside";
+  }
+  // Only change code above this line
+  return "Outside";
+}
+console.log(testLogicalOr(15));
+console.log(testLogicalOr(0));
+
